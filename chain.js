8ea5fa4f425c7721ed29d2994ea57a3f57c8514c
BravoSixOP <tyagisaksh2008@gmail.com>
2021-02-01 20:58:02 +0530

    Created Chain Class

--- chain.js
+++ chain.js
@@ -1,7 +1,7 @@
 class Chain {
     constructor(bodyA, bodyB){
         var options= {bodyA: bodyA, bodyB:bodyB, stiffness: 0.04, length: 10}
-        this.chain= constraint.create(options);
+        this.chain= Constraint.create(options);
         World.add(world, this.chain);
     }
     display(){
